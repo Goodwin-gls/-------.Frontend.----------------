cd9e6b5730e8e6f78d66ee1e43f97ee96487dece
Dmitry Tipugin <lifehackeromg@gmail.com>
2026-05-26 17:25:11 +0300

    e2e

--- frontend/cypress/e2e/workflow.cy.ts
+++ frontend/cypress/e2e/workflow.cy.ts
@@ -1,13 +1,13 @@
-describe('Workflow Editor E2E', () => {
+describe("Workflow Editor E2E", () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit("/");
+  });
 
-  it('should load the application', () => {
-    cy.contains('Workflow Editor').should('be.visible')
-  })
+  it("should load the application", () => {
+    cy.contains("Структура рабочего процесса").should("be.visible");
+  });
 
-  it('should load workflow steps from backend', () => {
-    cy.contains('Количество шагов').should('be.visible')
-  })
-})
+  it("should load workflow steps from backend", () => {
+    cy.get("table").should("be.visible");
+  });
+});
